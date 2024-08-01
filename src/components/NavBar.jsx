89f68340fee312f740a17fd6a5ea278252fbc97f
Nimasha Nimnadi <nimnadiweerakoon0987@gmail.com>
2024-08-02 02:24:51 +0530

    creating navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import logo from '../assets/NW.png';
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-900 mb-10 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" className="h-12 w-12 object-contain" />
+      </div>
+      <div className="flex space-x-4 m8 flex items-center justify-center gap-4">
+        <FaLinkedin className="text-white " />
+        <FaGithub className="text-white" />
+        <FaTwitter className="text-white" />
+        <FaInstagram className="text-white" />
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
