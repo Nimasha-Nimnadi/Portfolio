4cbb5b6a561c19a60c82d740ae6b2e0a68685ed6
Nimasha Nimnadi <nimnadiweerakoon0987@gmail.com>
2024-08-08 20:04:44 +0530

    tech
    Signed-off-by: Nimasha Nimnadi <nimnadiweerakoon0987@gmail.com>

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,7 +1,13 @@
+import {RiReactjsLine } from "react-icons/ri"
 const Technologies = () => {
     return (
       <div className="border-b border-neutral-800 pb-24">
-          <h1 className="">Technologies</h1>
+          <h1 className="my-20 text-center text-4xl">Technologies</h1>
+          <div className="flex flex-wrap items-center justify-center gap-4">
+            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+              <RiReactjsLine/>
+            </div>  
+          </div>
         
       </div>
     )
